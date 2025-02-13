768cf6faa93d295efd97c3c5f7131db0b0f9b822
Igor.Phillipe <igor.nascimento.aec@gmail.com>
2025-02-13 12:00:25 -0300

    Final da aula 120

--- src/application01/Program.java
+++ src/application01/Program.java
@@ -1,9 +1,25 @@
 package application01;
 
+import java.util.Date;
+
+import entites.enums01.OrderStatus;
+import entites01.Order;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
+			
+			Order order = new Order(1080,new Date(),OrderStatus.DELIVERED);
+			
+			System.out.println(order);
+			
+			OrderStatus os1 = OrderStatus.DELIVERED;
+			OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+			
+			
+			System.out.println(os1);
+			System.out.println(os2);
+			
 
 	}
 
---
+++ src/entites/enums01/OrderStatus.java
@@ -0,0 +1,10 @@
+package entites.enums01;
+
+public enum OrderStatus {
+		
+	PEDINNG_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+	
+}
